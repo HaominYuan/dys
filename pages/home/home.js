57f87c7c1da82d10ed0d7d57a68b55e9a5df87cb
tstxxy <tstxxy@gmail.com>
2020-08-02 00:09:05 +0800

    前后端联调

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,10 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
   },
 
-  disctap: function() {
+  textTap: function () {
+    console.log(this.data.text)
     let data = {
       user: 'yhm',
       age: '18',
@@ -18,6 +18,24 @@ Page({
     data = JSON.stringify(data)
     wx.navigateTo({
       url: `../disc/disc?data=${data}`
+    })
+  },
+
+  onLoad: function (options) {
+    const that = this
+    wx.request({
+      url: 'http://localhost:3000/',
+      data: {
+
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        that.setData({
+          text: res.data
+        })
+      }
     })
   }
 })
