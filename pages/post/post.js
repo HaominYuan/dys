ac9e93240651fff1d474f0ff215ec478b332a0fe
tstxxy <tstxxy@gmail.com>
2020-08-23 23:31:32 +0800

    上传图片修改

--- pages/post/post.js
+++ pages/post/post.js
@@ -1,55 +1,32 @@
 Page({
     data: {
-        filesUrl: []
+        images: ["./1.jpg"],
+        display: "inline-block"
     },
 
-    onLoad() {
-        this.setData({
-            selectFile: this.selectFile.bind(this),
-            uplaodFile: this.uplaodFile.bind(this)
-        })
-    },
-    chooseImage: function (e) {
-        var that = this;
+    add: function (e) {
+        let that = this
         wx.chooseImage({
-            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            count: 1,
             success: function (res) {
-                // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+                let images = that.data.images.concat(res.tempFilePaths)
+                console.log(images)
                 that.setData({
-                    files: that.data.files.concat(res.tempFilePaths)
-                });
+                    images
+                })
+                console.log(that.data)
             }
         })
     },
-    previewImage: function(e) {
-        wx.previewImage({
-            current: e.currentTarget.id, // 当前显示图片的http链接
-            urls: this.data.files // 需要预览的图片http链接列表
-        })
-    },
-    selectFile(files) {
-    },
-    uplaodFile(files) {
-        // 文件上传的函数，返回一个promise
-        return new Promise((resolve, reject) => {
-            const tempFilePaths = files.tempFilePaths;
-            let filesUrl = this.data.filesUrl.concat(tempFilePaths)
+    post: function (e) {
+        if (this.data.display === "none") {
             this.setData({
-                filesUrl: filesUrl
+                display: "inline-block"
             })
-            let object = {}
-            object['urls'] = tempFilePaths
-            resolve(object)
-        })
-    },
-    uploadError(e) {
-        console.log('upload error', e.detail)
-    },
-    uploadSuccess(e) {
-        console.log('upload success', e.detail)
-    },
-    post: function(e) {
-        console.log(this.data)
+        } else {
+            this.setData({
+                display: "none"
+            })
+        }
     }
 })
